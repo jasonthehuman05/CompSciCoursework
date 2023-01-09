32bdabc6bc6da4ea26e9a293278db2faf33f6beb
jasonthehuman05 <jason.the.human2005@gmail.com>
2023-01-09 16:29:54 +0000

    Split loader into separate function to class builder Started function to load index for tags

--- CourseworkPrototype/StockDatabase.py
+++ CourseworkPrototype/StockDatabase.py
@@ -4,9 +4,26 @@ class StockDatabase():
     tagIndex = dict()
     titleIndex = dict()
 
-    def __init__(self):
-        #Load the json file as a string
+    def __init__(self): #Runs when an object is made
+        self.LoadFile()
+
+    def LoadFile(self):
+        #Ensure all the lists are clear
+        self.database = dict()
+        self.tagIndex = dict()
+        self.titleIndex = dict()
+
+        #Load JSON File
         with open("stockdb.json") as file:
             dbjson = file.read()
             #parse the file
             self.database = json.loads(dbjson)
+
+    def IndexDatabaseTags(self):
+        #Ensure the tag index is empty
+        self.tagIndex = dict()
+
+        #Iterate through each item in stock
+        for index in range(len(self.database["items"])):
+            for tag in self.database["items"][index]["productTags"]:
+                #TODO: CHECK TAGS, ADD INDEX
