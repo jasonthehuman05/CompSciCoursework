4d428bcd66a61bf2f064b5b9d048b7c9dc8faaaf
jasonthehuman05 <jason.the.human2005@gmail.com>
2023-01-10 11:43:39 +0000

    Added title indexer

--- CourseworkPrototype/StockDatabase.py
+++ CourseworkPrototype/StockDatabase.py
@@ -8,6 +8,7 @@ class StockDatabase():
     def __init__(self): #Runs when an object is made
         self.LoadFile()
         self.IndexDatabaseTags()
+        self.IndexDatabaseTitles()
 
     def LoadFile(self):
         #Ensure all the lists are clear
@@ -35,3 +36,18 @@ class StockDatabase():
                 else:
                     self.tagIndex[tag] = [index]
         print(self.tagIndex)
+        
+    def IndexDatabaseTitles(self):
+        #Ensure the tag index is empty
+        self.titleIndex = dict()
+
+        #Iterate through each item in stock
+        for index in range(len(self.database["items"])):
+            for tag in self.database["items"][index]["productName"].split(" "):
+                tag = ''.join(c for c in tag if c.isalnum())
+                if tag in self.titleIndex:
+                    #Tag is already stored, add the index to the dict
+                    self.titleIndex[tag].append(index)
+                else:
+                    self.titleIndex[tag] = [index]
+        print(self.titleIndex)
