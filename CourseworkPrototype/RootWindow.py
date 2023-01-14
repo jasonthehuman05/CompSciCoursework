850192c198f2756bbefd46c6fc75b650678cb5e5
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2023-01-14 17:49:32 +0000

    Added button generator for items

--- CourseworkPrototype/RootWindow.py
+++ CourseworkPrototype/RootWindow.py
@@ -5,12 +5,17 @@ class RootWindow:
     pageNumber = 1
     pageCount = 18
 
+    itemNameLabel = []
+    itemNumberLabel = []
+    itemOpenButton = []
+
     def __init__(self):
         self.root = tkinter.Tk()
         self.root.title("BuildrightDB Prototype: Inventory Manager")
         self.DrawWidgets()
         self.root.geometry("600x400")
         self.root.mainloop()
+        
     def DrawWidgets(self):
         #CreateButton
         self.createButton = tkinter.Button(self.root, text="ADD INVENTORY", command= lambda:self.AddInventory())
@@ -23,21 +28,27 @@ class RootWindow:
         self.inventoryFrame = tkinter.Frame(self.root)
         self.inventoryFrame.configure(background='#aaaaaa')
         self.inventoryFrame.pack()
-        self.inventoryFrame.place(x=8, y=48, width = 584, height=300, anchor="nw")
+        self.inventoryFrame.place(x=8, y=48, width = 584, height=306, anchor="nw")
 
-        self.LoadPage()
+        self.GenerateItemInformation()
 
         self.pageLabel = tkinter.Label(self.root, text=f"Page {self.pageNumber} of {self.pageCount}")
-        self.pageLabel.place(x=300, y=360, anchor="center")
+        self.pageLabel.place(x=300, y=366, anchor="center")
         
         self.prevPageButton = tkinter.Button(self.root, text="<=", command=lambda:self.ChangePage(-1))
-        self.prevPageButton.place(x=8, y=348)
+        self.prevPageButton.place(x=8, y=354)
 
         self.nextPageButton = tkinter.Button(self.root, text="=>", command=lambda:self.ChangePage(1))
-        self.nextPageButton.place(x=565, y=348)
+        self.nextPageButton.place(x=565, y=354)
 
     def GenerateItemInformation(self):
         #Generates the holders for the item information
+        marginBetweenRows = 8
+        elementHeight = 8
+        for i in range(0, 12):
+            #generate buttons
+            self.itemNameLabel.append(tkinter.Button(self.inventoryFrame, text="View Details", command=lambda: print("Unassigned!")))
+            self.itemNameLabel[i].place(x=8, y=((18+marginBetweenRows)*i)+2, height=18, width=100)
         pass
 
     def ChangePage(self, direction:int):
