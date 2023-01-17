e54319545cdc83c8278ed1a7d76c24461687e0fc
jasonthehuman05 <jason.the.human2005@gmail.com>
2023-01-17 11:05:04 +0000

    reformatted the variation builder so each item is part of an array Started logic for saver

--- CourseworkPrototype/DetailsViewer.py
+++ CourseworkPrototype/DetailsViewer.py
@@ -21,67 +21,80 @@ class DetailsViewer:
         
     def DrawWidgets(self):
         #Save Button
-        saveButton = tkinter.Button(self.root, text="SAVE", command=lambda: self.Passer())
+        saveButton = tkinter.Button(self.root, text="SAVE", command=lambda: self.SaveChanges())
         saveButton.place(x=0,y=0,width=100,height=25)
 
         #Item Name
-        itemNameEntry = tkinter.Entry(self.root)
-        itemNameLabel = tkinter.Label(self.root, text="Item Name")
-        itemNameEntry.insert(0, self.item["productName"])
-        itemNameEntry.place(x=75, y=25, width=300)
-        itemNameLabel.place(x=0, y=25)
+        self.itemNameEntry = tkinter.Entry(self.root)
+        self.itemNameLabel = tkinter.Label(self.root, text="Item Name")
+        self.itemNameEntry.insert(0, self.item["productName"])
+        self.itemNameEntry.place(x=75, y=25, width=300)
+        self.itemNameLabel.place(x=0, y=25)
 
         #Tags
-        itemTagEntry = tkinter.Entry(self.root)
-        itemTagLabel = tkinter.Label(self.root, text="Item Tags")
+        self.itemTagEntry = tkinter.Entry(self.root)
+        self.itemTagLabel = tkinter.Label(self.root, text="Item Tags")
 
         tags = ""
         for tag in self.item["productTags"]:
             tags += f"{tag}, "
         tags = tags[:-2] #removes ", " from tag string
 
-        itemTagEntry.insert(0, tags)
-        itemTagEntry.place(x=75, y=50, width=300)
-        itemTagLabel.place(x=0, y=50)
+        self.itemTagEntry.insert(0, tags)
+        self.itemTagEntry.place(x=75, y=50, width=300)
+        self.itemTagLabel.place(x=0, y=50)
 
         #Variations
-        itemVariationsHeaderLabel = tkinter.Label(self.root, text="Item Variations")
-        itemVariationsHeaderLabel.place(x=0, y=75)
+        self.itemVariationsHeaderLabel = tkinter.Label(self.root, text="Item Variations")
+        self.itemVariationsHeaderLabel.place(x=0, y=75)
         self.DrawVariations()
 
     def DrawVariations(self):
         #Make header labels
-        variationIDLabel = tkinter.Label(self.root, text="ID")
-        variationIDLabel.place(x=0, y=100)
-        variationNameLabel = tkinter.Label(self.root, text="Name")
-        variationNameLabel.place(x=50, y=100)
-        variationCostLabel = tkinter.Label(self.root, text="Cost")
-        variationCostLabel.place(x=300, y=100)
-        variationStockLabel = tkinter.Label(self.root, text="Stock")
-        variationStockLabel.place(x=350, y=100)
+        self.variationIDLabel = tkinter.Label(self.root, text="ID")
+        self.variationIDLabel.place(x=0, y=100)
+        self.variationNameLabel = tkinter.Label(self.root, text="Name")
+        self.variationNameLabel.place(x=50, y=100)
+        self.variationCostLabel = tkinter.Label(self.root, text="Cost")
+        self.variationCostLabel.place(x=300, y=100)
+        self.variationStockLabel = tkinter.Label(self.root, text="Stock")
+        self.variationStockLabel.place(x=350, y=100)
         
         #Get Variations
         variations = self.item["variations"]
         
         yPos = 125 #start at y=125, since there are other labels to accomodate above
+        
+        #everything needed to put things into lists
+        varNum = 0
+        self.variationIDEntry = []
+        self.variationNameEntry = []
+        self.variationCostEntry = []
+        self.variationStockEntry =  []
+
         for variation in variations:
             #For each variation, display the information
-            variationIDEntry = tkinter.Entry(self.root)
-            variationIDEntry.insert(0, variation["variationID"])
-            variationIDEntry.place(x=0, y=yPos, width = 45)
+            self.variationIDEntry.append(tkinter.Entry(self.root))
+            self.variationIDEntry[varNum].insert(0, variation["variationID"])
+            self.variationIDEntry[varNum].place(x=0, y=yPos, width = 45)
             
-            variationNameEntry = tkinter.Entry(self.root)
-            variationNameEntry.insert(0, variation["variationName"])
-            variationNameEntry.place(x=50, y=yPos, width = 245)
+            self.variationNameEntry.append(tkinter.Entry(self.root))
+            self.variationNameEntry[varNum].insert(0, variation["variationName"])
+            self.variationNameEntry[varNum].place(x=50, y=yPos, width = 245)
             
-            variationCostEntry = tkinter.Entry(self.root)
-            variationCostEntry.insert(0, variation["variationCost"])
-            variationCostEntry.place(x=300, y=yPos, width = 45)
+            self.variationCostEntry.append(tkinter.Entry(self.root))
+            self.variationCostEntry[varNum].insert(0, variation["variationCost"])
+            self.variationCostEntry[varNum].place(x=300, y=yPos, width = 45)
             
-            variationStockEntry = tkinter.Entry(self.root)
-            variationStockEntry.insert(0, variation["stockLevel"])
-            variationStockEntry.place(x=350, y=yPos, width = 45)
+            self.variationStockEntry.append(tkinter.Entry(self.root))
+            self.variationStockEntry[varNum].insert(0, variation["stockLevel"])
+            self.variationStockEntry[varNum].place(x=350, y=yPos, width = 45)
             yPos+=25
+            varNum+=1
 
     def Passer(self):
         pass
+
+    def SaveChanges(self):
+        for i in range(0, len(self.variationIDEntry)):
+            print(self.variationIDEntry[i].get())
