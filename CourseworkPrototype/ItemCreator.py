109b926a3b6497d5eef1c82938cf28684b930d33
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2023-01-18 18:04:11 +0000

    Finished design of item creator

--- CourseworkPrototype/ItemCreator.py
+++ CourseworkPrototype/ItemCreator.py
@@ -9,10 +9,11 @@ class ItemCreator:
         self.db = db
         #Main Window
         self.root = tkinter.Tk()
+        self.root.resizable(False, False)
         self.root.title("New Item")
         self.DrawWidgets()
 
-        self.root.geometry(f"300x200")
+        self.root.geometry(f"300x225")
 
         self.root.mainloop()
         
@@ -31,6 +32,28 @@ class ItemCreator:
         self.tagsHelpLabel.place(x=75,y=50)
         self.tagsEntry.place(x=75,y=25,width=225)
 
-    
+        #Variation Creator
+        self.variationNoticeLabel = tkinter.Label(self.root, text="Add your first variation", foreground="red")
+        self.variationNameLabel = tkinter.Label(self.root, text="Variation Name")
+        self.variationCostLabel = tkinter.Label(self.root, text="Variation Cost")
+        self.variationStockLabel = tkinter.Label(self.root, text="Variation Stock")
+        
+        self.variationNameEntry = tkinter.Entry(self.root)
+        self.variationCostEntry = tkinter.Entry(self.root)
+        self.variationStockEntry = tkinter.Entry(self.root)
+
+        self.variationNoticeLabel.place(x=0,y=75)
+        self.variationNameLabel.place(x=0,y=100)
+        self.variationCostLabel.place(x=0,y=125)
+        self.variationStockLabel.place(x=0,y=150)
+        
+        self.variationNameEntry.place(x=100,y=100)
+        self.variationCostEntry.place(x=100,y=125)
+        self.variationStockEntry.place(x=100,y=150)
+
+        #Save Button
+        self.addItemButton = tkinter.Button(self.root, text="ADD ITEM", bg="lime", command=lambda:self.Passer())
+        self.addItemButton.place(x=0,y=175,width=300,height=50)
+
     def Passer(self):
         pass
