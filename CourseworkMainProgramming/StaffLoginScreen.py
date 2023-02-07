e8fd0bd30cd4a12b92365af6c1deb0e88cae0835
jasonthehuman05 <jason.the.human2005@gmail.com>
2023-02-07 12:04:23 +0000

    staff login system made

--- CourseworkMainProgramming/StaffLoginScreen.py
+++ CourseworkMainProgramming/StaffLoginScreen.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import messagebox
 import colorfile
 from databases import StockDatabase, CustomerDatabase, StaffDatabase, BasketDatabase, OrderDatabase
 import StaffPortal
@@ -25,12 +26,28 @@ class StaffLoginScreen:
         self.root.destroy()
 
     def DrawWidgets(self):
+        #username
+        self.usernameLabel = tkinter.Label(self.root, text="Username")
+        self.usernameLabel.place(x=8,y=40,width=92,height=26)
+        self.usernameEntry = tkinter.Entry(self.root)
+        self.usernameEntry.place(x=100,y=40,width=292,height=26)
+
+        #password
+        self.passwordLabel = tkinter.Label(self.root, text="Password")
+        self.passwordLabel.place(x=8,y=74,width=92,height=26)
+        self.passwordEntry = tkinter.Entry(self.root, show="\u2022")
+        self.passwordEntry.place(x=100,y=74,width=292,height=26)
+
         #Login Button
         self.loginButton = tkinter.Button(self.root, text="Log In", font="default 24 normal", command=lambda:self.TryLogin())
         self.loginButton.place(x=0,y=150,height=50,width=400)
 
     def TryLogin(self):
-        self.Login()
+        id = self.staffdb.LoginStaff(self.usernameEntry.get(), self.passwordEntry.get())
+        if id == "000000":
+            messagebox.showwarning("Incorrect Login", "The provided login details could not be found!")
+        else:
+            self.Login()
     
     def Login(self):
         self.root.withdraw()
