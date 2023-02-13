0b5c9b4b8043bb8ba10f87129cb4d6e90c3beebd
jasonthehuman05 <jason.the.human2005@gmail.com>
2023-02-13 15:51:12 +0000

    added staffmanager base window builder

--- CourseworkMainProgramming/staffViews/staffManager/StaffManager.py
+++ CourseworkMainProgramming/staffViews/staffManager/StaffManager.py
@@ -1,8 +1,26 @@
 import tkinter
 
 class StaffManager:
-    pass
+    def __init__(self, staffdb):
+        #Public vars
+        self.staffdb = staffdb
 
+        #Build Window
+        self.root = tkinter.Toplevel()
+        self.root.title("BuildrightDB Staff Manager")
+        self.root.geometry("500x400")
+        self.DrawWidgets()
+        self.root.mainloop()
+
+    def DrawWidgets(self):
+        self.addStaffButton = tkinter.Button(self.root, text="Add New Staff", command=lambda:self.AddStaff())
+        self.addStaffButton.place(x=0,y=0,width=100,height=24)
+
+    def AddStaff(self):
+        pass
+
+    def DisplayStaff(self):
+        pass
 
 
 
