1b9e9562220f7d2a07909b0a70ce2c687cbffeda
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2023-03-09 10:13:56 +0000

    Changed a comment

--- CourseworkMainProgramming/staffViews/stockManager/StockManager.py
+++ CourseworkMainProgramming/staffViews/stockManager/StockManager.py
@@ -1,6 +1,6 @@
-#This is the startfile
 from staffViews.stockManager import RootWindow
 
+#Creates instance of window from prototype. 
 class StockManager:
 	def __init__(self, db):
 		mainWin = RootWindow.RootWindow(db)
